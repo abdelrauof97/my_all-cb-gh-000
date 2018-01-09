9686680968603182053a274995764521ebe39ca7
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-09 19:53:53 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,7 +7,7 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i += 1
   end
-  block_return_values.include?
+  block_return_values.include
 end
 numbers = [1,2,3]
 binding.pry
